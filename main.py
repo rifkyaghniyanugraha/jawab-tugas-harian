47da9279cf26788dc5ac950d1a87bd07e333ee2b
r.a.nugraha <rifky.a.nugraha@gmail.com>
2025-07-20 22:44:27 +0700

    Studi 1

---
+++ main.py
@@ -0,0 +1,9 @@
+username = str(input("Masukkan username Anda: "))
+password = str(input("Masukkan password Anda: "))
+
+if username == "admin" and password == "123":
+    print("Login Berhasil")
+else:
+    print("Login Gagal")
+    
+    
