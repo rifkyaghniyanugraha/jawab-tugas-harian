0e38199dbd1af258a3e1f25095be20b757509702
r.a.nugraha <rifky.a.nugraha@gmail.com>
2025-07-21 13:35:12 +0700

    Studi Kasus 2

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+"""
+STUDI KASUS: 1
+
 username = str(input("Masukkan username Anda: "))
 password = str(input("Masukkan password Anda: "))
 
-if username == "admin" and password == "123":
-    print("Login Berhasil")
-else:
-    print("Login Gagal")
-    
-    
+print("Login Berhasil")
+
+"""
+
+
+"""
+STUDI KASUS: 2
+
+valid_username = "admin"
+valid_password = "1234"
+percobaan = 0
+
+while True:
+    if percobaan < 3:
+        input_username = str(input("Masukkan username Anda: "))
+        input_password = str(input("Masukkan password Anda: "))
+        percobaan += 1
+
+        if input_username == valid_username and input_password == valid_password:
+            print("Login Berhasil")
+            break
+        else:
+            print("Login Gagal")
+    else:
+        print("Akun Terkunci")
+        break
+"""
+
